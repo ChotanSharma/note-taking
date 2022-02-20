8e0e318d9e9174931d7237724e6b441f0b85d8cc
ChotanSharma <chotansharma@yahoo.com>
2022-02-20 01:27:50 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// require fs to update the data in local
+const fs = require('fs');
+const path = require('path');
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -10,7 +14,28 @@ const { notes } = require('./data/db');
 
 const PORT = process.env.PORT || 3001;
 
+function createNewData(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
 
+    fs.writeFileSync(
+        path.join(__dirname, './data/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    return note;
+}
+
+
+function validateData(note) {
+    if (!note.title || typeof note.title !== 'string') {
+      return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+      return false;
+    }
+    return true;
+}
 
 app.get('/api/notes', (req, res) => {
     let results = notes;
@@ -22,8 +47,17 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if(!validateData(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const note = createNewData(req.body, notes);
+        res.json(note);
+    }
 });
 
 
