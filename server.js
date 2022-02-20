2db39332abaa7f77bf99df992d421a02191fc827
ChotanSharma <chotansharma@yahoo.com>
2022-02-20 12:27:03 -0500

    refresh the codes

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
-// require fs to update the data in local
-const fs = require('fs');
-const path = require('path');
+const { notes } = require('./data/db');
+
+// middleware to instruct server certain files available
+app.use(express.static('public'));
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-const { notes } = require('./data/db');
-
 const PORT = process.env.PORT || 3001;
 
+// create new note and save
 function createNewData(body, notesArray) {
     const note = body;
     notesArray.push(note);
@@ -26,7 +29,7 @@ function createNewData(body, notesArray) {
     return note;
 }
 
-
+// validate the note input
 function validateData(note) {
     if (!note.title || typeof note.title !== 'string') {
       return false;
@@ -35,6 +38,21 @@ function validateData(note) {
       return false;
     }
     return true;
+}
+
+
+// delete notes
+function deleteNote(id, notesArray) {
+    const noteIndex = id - 1;
+    notesArray.splice(noteIndex, 1);
+    notesArray.map((note, index) => {
+        note.id = index.toString();;
+    })
+    fs.writeFileSync(
+        path.join(__dirname, "./data/db.json"),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return notesArray;
 }
 
 app.get('/api/notes', (req, res) => {
@@ -60,7 +78,27 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+//request to delete note by id
+app.delete('/api/notes/:id', (req, res) => {
+    const result = deleteNote(req.params.id, notes);
+    res.json(result);
+});
+
+// route to create homepage for server
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// wildcard route to check bad request
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
-  });
+    console.log(`API server now on port ${PORT}!`);
+});
